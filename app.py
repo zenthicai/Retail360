92b7f982268437a57fbd9768b93336eab2453466
zenthicai <durga@zenthicai.com>
2025-07-30 19:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from PIL import Image
 import numpy as np
 #import cv2
 #from pyzbar.pyzbar import decode
-from paddleocr import PaddleOCR
+import pytesseract
 from collections import defaultdict
 from datetime import datetime
 import tempfile
